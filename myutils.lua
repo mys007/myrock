ed3315d4791b123c3f11c509dc265e51a3efab58
Martin Simonovsky <simonovm@imagine.enpc.fr>
2015-06-08 10:47:53 +0200

    - added string split

--- myutils.lua
+++ myutils.lua
@@ -22,6 +22,31 @@ function string.trim(s)
   return (s:gsub("^%s*(.-)%s*$", "%1"))
 end
 
+function string.split(str, delim, maxNb)
+    -- Eliminate bad cases...
+    if string.find(str, delim) == nil then
+        return { str }
+    end
+    if maxNb == nil or maxNb < 1 then
+        maxNb = 0    -- No limit
+    end
+    local result = {}
+    local pat = "(.-)" .. delim .. "()"
+    local nb = 0
+    local lastPos
+    for part, pos in string.gfind(str, pat) do
+        nb = nb + 1
+        result[nb] = part
+        lastPos = pos
+        if nb == maxNb then break end
+    end
+    -- Handle the last field
+    if nb ~= maxNb then
+        result[nb + 1] = string.sub(str, lastPos)
+    end
+    return result
+end
+
 ----------------------------------------------------------------------
 --Creates isotropic 2D gaussian. Unlike image.gaussian this is consistent with matlab's fspecial
 function gaussianfilter2D(size, sigma)
@@ -56,6 +81,8 @@ function funcOnTensors(input, f)
     end
 end
 
+----------------------------------------------------------------------
+-- Returns a string of tensor dimension AxBxCx..
 function formatSizeStr(input)
     local function sizeStr(x) 
         if x:nDimension() == 0 then
