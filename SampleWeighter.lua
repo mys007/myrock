96f9c2ac5a319949050a59fb53b7100ea272763e
Martin Simonovsky <simonovm@imagine.enpc.fr>
2016-01-05 21:22:14 +0100

    added sampleweigher (from locate)

---
+++ SampleWeighter.lua
@@ -0,0 +1,29 @@
+require 'nn'
+
+-----------
+-- Weights backpropagation of different samples in batch by specified factors.
+local SampleWeighter, SampleWeighter_parent = torch.class('myrock.SampleWeighter', 'nn.Module')
+
+function SampleWeighter:__init()
+    SampleWeighter_parent.__init(self)
+    self.factors = torch.Tensor() --set externally
+end
+
+function SampleWeighter:updateOutput(input)
+   self.output = input
+   return self.output
+end
+
+function SampleWeighter:updateGradInput(input, gradOutput)
+    local factors = gradOutput:dim()==2 and self.factors:view(-1,1) or self.factors
+    local L1orig = gradOutput:norm(1)
+    gradOutput:cmul(factors:expandAs(gradOutput))
+    local L1new = gradOutput:norm(1)
+    gradOutput:mul(L1orig/L1new) --keep the L1-norm of gradient ("it's energy") for consistency
+    self.gradInput = gradOutput
+    return self.gradInput
+end
+
+function SampleWeighter:setFactors(factors)
+    self.factors:resize(factors:size()):copy(factors)
+end
--- myrock.lua
+++ myrock.lua
@@ -4,6 +4,7 @@ require 'myutils'
 require 'CmdLine'
 require 'CudaAdapter'
 require 'DebugModule' 
+require 'SamplerWeighter' 
 --TODO: unify namespace and include more files?
 
 return myrock
